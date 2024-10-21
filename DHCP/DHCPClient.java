cb3540fad6bc5b107d3eabe6d5ffe9aae2ead24f
Harshavardhan2099 <tonyharshavardhan1@gmail.com>
2024-10-21 21:16:03 +0530

    DHCP was corrected

--- DHCP/DHCPClient.java
+++ DHCP/DHCPClient.java
@@ -1,51 +1,53 @@
 import java.net.*;
 import java.util.Scanner;
 
-
 public class DHCPClient {
     public static void main(String[] args) throws Exception {
         DatagramSocket clientSocket = new DatagramSocket();
         InetAddress serverAddress = InetAddress.getByName("localhost");
-        byte[] sendData; 
+        byte[] sendData;
         byte[] receiveData = new byte[1024];
 
+        @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter client MAC Address: ");
-        String clientMac = sc.nextLine(); 
+        System.out.print("Enter client MAC address: ");
+        String clientMAC = sc.nextLine();
 
-        // DHCP DISCOVER 
-        String discoverMessage = "DHCPDISCOVER: " + clientMac; 
-        sendData = discoverMessage.getBytes(); 
+        // Step 1: DHCP DISCOVER
+        String discoverMessage = "DHCPDISCOVER: " + clientMAC;
+        sendData = discoverMessage.getBytes();
         DatagramPacket discoverPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 9876);
         clientSocket.send(discoverPacket);
         System.out.println("Sent DHCPDISCOVER");
 
-        // DHCP OFFER 
+        // Step 2: Receive DHCP OFFER
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         clientSocket.receive(receivePacket);
         String offerMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
         System.out.println("Received: " + offerMessage);
+
         String[] offerParts = offerMessage.split(":");
         String assignedIP = offerParts[1].trim();
-        
-        // DHCP REQUEST
-        String requestMessage = "DHCPREQUEST: " + clientMac + ": " + assignedIP; 
-        sendData = requestMessage.getBytes(); 
+
+        // Step 3: DHCP REQUEST
+        String requestMessage = "DHCPREQUEST: " + clientMAC + ": " + assignedIP;
+        sendData = requestMessage.getBytes();
         DatagramPacket requestPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 9876);
         clientSocket.send(requestPacket);
         System.out.println("Sent DHCPREQUEST for IP: " + assignedIP);
 
-        // DHCP ACK 
+        // Step 4: Receive DHCP ACK
         receivePacket = new DatagramPacket(receiveData, receiveData.length);
         clientSocket.receive(receivePacket);
         String ackMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
         System.out.println("Received: " + ackMessage);
-        if(ackMessage.startsWith("DHCPACK")) {
+
+        if (ackMessage.startsWith("DHCPACK")) {
             System.out.println("IP Address assigned successfully: " + assignedIP);
         } else {
-            System.out.println("Failed to assign IP address");
+            System.out.println("Failed to obtain IP address.");
         }
-        sc.close();
-        clientSocket.close(); 
+
+        clientSocket.close();
     }
 }
--- DHCP/DHCPServer.java
+++ DHCP/DHCPServer.java
@@ -2,61 +2,70 @@ import java.net.*;
 import java.util.HashMap;
 
 public class DHCPServer {
-    private static HashMap<String, String> ipPool = new HashMap<>();
-    private static String subnet = "192.168.1";
-    private static int nextIP = 100; 
-    
+    private static HashMap<String, String> ipPool = new HashMap<>();  // Maps MAC addresses to IP addresses
+    private static String subnet = "192.168.1.";
+    private static int nextIP = 100;  // Start assigning IPs from 192.168.1.100
+
     public static void main(String[] args) throws Exception {
         @SuppressWarnings("resource")
-        DatagramSocket serverSocket = new DatagramSocket(9876); 
+        DatagramSocket serverSocket = new DatagramSocket(9876);
         byte[] receiveData = new byte[1024];
-        byte[] sendData; 
+        byte[] sendData;
 
         System.out.println("DHCP Server is running...");
+
         while (true) {
+            // Receive client request
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
             String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
             System.out.println("Received: " + clientMessage);
-            InetAddress clientAddress = receivePacket.getAddress();
-            int clientPort = receivePacket.getPort(); 
-            String[] parts = clientMessage.split(":");
-            String messageType = parts[0].trim();
-            String clientMac = parts[1].trim(); 
-            String assignedIP = ipPool.get(clientMac);
 
+            InetAddress clientIPAddress = receivePacket.getAddress();
+            int clientPort = receivePacket.getPort();
+            String[] parts = clientMessage.split(":");
+
+            String messageType = parts[0].trim();
+            String clientMAC = parts[1].trim();
+
+            String assignedIP = ipPool.get(clientMAC);
+
+            // Handle DHCP message types
             switch (messageType) {
                 case "DHCPDISCOVER":
+                    // DHCP DISCOVER: Offer a new IP if the client doesn't already have one
                     if (assignedIP == null) {
-                        assignedIP = assignIP(clientMac);
+                        assignedIP = assignIP(clientMAC);
                     }
-                    String offerMessage = "DHCPOFFER: " + assignedIP; 
+                    String offerMessage = "DHCPOFFER: " + assignedIP;
                     sendData = offerMessage.getBytes();
-                    DatagramPacket offerPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+                    DatagramPacket offerPacket = new DatagramPacket(sendData, sendData.length, clientIPAddress, clientPort);
                     serverSocket.send(offerPacket);
-                    System.out.println("Offered IP: " + assignedIP + " to client with MAC :" + clientMac);
+                    System.out.println("Offered IP: " + assignedIP + " to client with MAC: " + clientMAC);
                     break;
-                
+
                 case "DHCPREQUEST":
+                    // DHCP REQUEST: Confirm the offered IP and send an acknowledgment
                     String requestedIP = parts[2].trim();
-                    if (requestedIP == assignedIP) {
-                        String ackMessage = "DHCPACK: " + requestedIP; 
-                        sendData = ackMessage.getBytes(); 
-                        DatagramPacket ackPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort); 
+                    if (requestedIP.equals(assignedIP)) {
+                        String ackMessage = "DHCPACK: " + requestedIP;
+                        sendData = ackMessage.getBytes();
+                        DatagramPacket ackPacket = new DatagramPacket(sendData, sendData.length, clientIPAddress, clientPort);
                         serverSocket.send(ackPacket);
-                        System.out.println("Assigned IP: " + requestedIP + " to client with MAC: " + clientMac);
-                    } 
+                        System.out.println("Assigned IP: " + requestedIP + " to client with MAC: " + clientMAC);
+                    }
                     break;
+
                 default:
-                    System.out.println("Unknown DHCP message type");
-                    break;
+                    System.out.println("Unknown DHCP message type.");
             }
         }
     }
 
+    // Method to assign a new IP
     private static String assignIP(String macAddress) {
         String newIP = subnet + nextIP;
-        nextIP++; 
+        nextIP++;
         ipPool.put(macAddress, newIP);
         return newIP;
     }
